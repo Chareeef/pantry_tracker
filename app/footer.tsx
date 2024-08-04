26b22879417abb69280771ac690a3f07efc10a12
Chareeef <youssef.charif.h@gmail.com>
2024-08-04 15:51:48 +0000

    Enhance footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,12 +1,13 @@
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-center p-2 bg-lime-500 border-t-2 border-lime-500 text-white text-sm">
+    <footer className="flex flex-col justify-center items-center p-4 bg-lime-500 border-t-2 border-lime-500 text-white text-xl">
       <h3 className="font-bold">Youssef Charif Hamidi</h3>
       <p className="mt-1">© 2024</p>
       <div className="flex space-x-4 mt-2">
         <a
+          className="hover:text-green-800"
           href="https://github.com/Chareeef"
           target="_blank"
           rel="noopener noreferrer"
@@ -14,6 +15,7 @@ export default function Footer() {
           <FaGithub size={20} />
         </a>
         <a
+          className="hover:text-green-800"
           href="https://linkedin.com/in/youssef-charif-hamidi"
           target="_blank"
           rel="noopener noreferrer"
@@ -24,7 +26,7 @@ export default function Footer() {
           href="https://x.com/YoussefCharifH2"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center hover:text-green-800"
           style={{ width: "20px", height: "20px" }}
         >
           <span style={{ fontSize: "20px", fontWeight: "bold" }}>𝕏</span>
